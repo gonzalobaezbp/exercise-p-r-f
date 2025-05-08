cd367d3f020ffd7ca02492914207ddb7f8289650
Gonzalo Baez <gonzalobaezbp@gmail.com>
2025-05-08 12:12:38 -0500

    P1 - Model

---
+++ domain/model/src/main/java/co/com/bancolombia/model/account/Account.java
@@ -0,0 +1,18 @@
+package co.com.bancolombia.model.account;
+import lombok.Builder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+//import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+//@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class Account {
+
+    private final Long id;
+    private final String name;
+
+}
